0def6e5b0275a95182d0a39aef59ee41a06db532
sangaviravi10 <53516901+sangaviravi10@users.noreply.github.com>
2019-07-31 17:40:08 +0530

    Update Runner.java

--- src/test/java/Stepdef/Runner.java
+++ src/test/java/Stepdef/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Scenario/Scenario2.feature",plugin="html:target\\htmlreport")
+@CucumberOptions(features="src/test/resources/Scenario/Scenario2.feature",plugin="json:target\\jsonreport.json")
 public class Runner {
 	
 
